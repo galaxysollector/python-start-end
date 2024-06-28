b8ee74cb5732e95a4c3eb7b06277b46b918c78a9
galaxysollector <99175924+galaxysollector@users.noreply.github.com>
2024-06-28 20:33:36 +0900

    Add files via upload

---
+++ python-start-end-linux.py
@@ -0,0 +1,23 @@
+﻿import os
+import time
+
+os.system('clear')    #Clear terminal on first run
+
+version='0.0.0'
+print('TEST [Version',version+']')
+print('(c) galaxysollector. All rights reserved.')
+time.sleep(2)
+
+os.system('clear')    #Clear terminal to print next screen
+time.sleep(1)
+
+#Start of code
+
+#Write your own code
+
+#End of code
+
+print('Exiting...')
+time.sleep(1)
+
+os.system('clear')    #Clear terminal and exit
